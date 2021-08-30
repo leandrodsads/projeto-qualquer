bc22adf625fbc7fc4da301ccf7d779310940e7be
leandrodsads <85911683+leandrodsads@users.noreply.github.com>
2021-08-30 14:41:59 -0300

    DC - Desenvolvimento em PHP - 2
    DC - Desenvolvimento em PHP - 2

---
+++ projeto-qualquer/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'leandrosilva/projeto-qualquer',
+  ),
+  'versions' => 
+  array (
+    'leandrosilva/projeto-qualquer' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'wead/digital-cep' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '63b5624635c0c1e0f2020f672dcfd235dd9834ff',
+    ),
+  ),
+);
